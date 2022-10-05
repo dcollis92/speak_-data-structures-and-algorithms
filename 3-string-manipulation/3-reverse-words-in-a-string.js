8d6800cb1b0905548a53a68bce52d87d122bac0b
dcollis92 <davidphilipcollis@gmail.com>
2022-10-05 11:32:27 -0500

    valid palindrome solution

---
+++ 3-string-manipulation/3-reverse-words-in-a-string.js
@@ -0,0 +1,36 @@
+/*
+ * 557. Reverse Words in a String III
+ *
+ * Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+ *
+ * Example 1:
+ * Input: s = "Let's take LeetCode contest"
+ * Output: "s'teL ekat edoCteeL tsetnoc"
+ *
+ * Example 2:
+ * Input: s = "God Ding"
+ * Output: "doG gniD"
+ *
+ * Constraints:
+ * 1 <= s.length <= 5 * 10**4
+ * s contains printable ASCII characters.
+ * s does not contain any leading or trailing spaces.
+ */
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function (s) {
+  return (
+    s
+      .split(" ")
+      // Split the string into an array of words
+      .map(
+        // Map each word to a reversed version of itself
+        (word) => word.split("").reverse().join("")
+      )
+      .join(" ")
+  );
+  // Join the array of reversed words into a string
+};
